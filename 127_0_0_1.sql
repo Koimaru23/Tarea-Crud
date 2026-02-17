124ad3c2f54cd37efcd35d3a487ea7e3d2c1f4f1
Hector <paamw7w7@gmail.com>
2026-02-17 12:48:58 -0500

    Script Base de Datos

---
+++ 127_0_0_1.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-02-2026 a las 18:45:34
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crud-php-mysql`
+--
+CREATE DATABASE IF NOT EXISTS `crud-php-mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `crud-php-mysql`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Creación: 17-02-2026 a las 17:02:22
+-- Última actualización: 17-02-2026 a las 17:28:18
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `passowrd` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `name`, `lastname`, `username`, `passowrd`, `email`) VALUES
+(1, 'pedro', 'claudi', 'claudiew', '123', 'pedro@email.com'),
+(3, 'maria', 'claudi', 'adar', '123', 'mari@email.com'),
+(4, 'luciana', 'maria', 'mariaewe', '123', 'mari@email.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
